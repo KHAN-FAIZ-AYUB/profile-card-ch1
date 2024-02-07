334b96f2ee48aa880073eda9fab9f937dc362bde
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-07 22:20:18 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <Image />
+      <Heading />
+      <Intro />
+      <Skills skill="HTML/CSS" />
+      <Skills skill="JavaScript" />
+      <Skills skill="React" />
+      <Skills skill="Git & GitHub" />
+      <Skills skill="Next.Js" />
+    </div>
+  );
+}
+
+function Image() {
+  return <img src="logo192.png" alt="avtar"></img>;
+}
+
+function Heading() {
+  return <h2>FAIZ AYUB KHAN</h2>;
+}
+
+function Intro() {
+  return <p>Full-stack developer working in XYZ company Since 1998</p>;
+}
+
+function Skills(props) {
+  return (
+    <div>
+      <p>{props.skill}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

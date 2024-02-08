3ab3e120ea24ef46d82850232ce6ee3a12b364c2
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-08 14:29:07 +0530

    avtar

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <Image />
-      <Heading />
-      <Intro />
-      <Skills skill="HTML/CSS" />
-      <Skills skill="JavaScript" />
-      <Skills skill="React" />
-      <Skills skill="Git & GitHub" />
-      <Skills skill="Next.Js" />
+    <div className="card">
+      <Avtar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
     </div>
   );
 }
 
-function Image() {
-  return <img src="logo192.png" alt="avtar"></img>;
-}
-
-function Heading() {
-  return <h2>FAIZ AYUB KHAN</h2>;
+function Avtar() {
+  return <img className="avatar" src="PR_IMG.jpg" alt="avtar"></img>;
 }
 
 function Intro() {
   return <p>Full-stack developer working in XYZ company Since 1998</p>;
 }
 
-function Skills(props) {
-  return (
-    <div>
-      <p>{props.skill}</p>
-    </div>
-  );
+function SkillList() {
+  return;
+}
+
+function skill() {
+  return;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
